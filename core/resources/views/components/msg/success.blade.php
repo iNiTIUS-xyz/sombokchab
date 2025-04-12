9527c6db147bf76a2464617837bd887d83a088f9
Masud Rana Tapu <masudranatapu@gmail.com>
2025-04-12 16:16:45 +0600

    done

--- core/resources/views/components/msg/success.blade.php
+++ core/resources/views/components/msg/success.blade.php
@@ -1,5 +1,6 @@
-@if(session()->has('msg'))
-    <div class="alert alert-{{session('type') ? session('type') : 'success' }}">
+@if (session()->has('msg'))
+    <div class="alert alert-{{ session('type') ? session('type') : 'success' }} alert-dismissible fade show">
         {!! Purifier::clean(session('msg')) !!}
+        <button type="button btn-sm" class="close" data-bs-dismiss="alert">X</button>
     </div>
 @endif
