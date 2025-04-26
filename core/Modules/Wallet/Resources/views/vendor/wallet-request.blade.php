ce82b5acf4203e842d5fd983d9713a2dd837fb6b
Masud Rana Tapu <masudranatapu@gmail.com>
2025-04-26 12:09:22 +0600

    working on it

--- core/Modules/Wallet/Resources/views/vendor/wallet-request.blade.php
+++ core/Modules/Wallet/Resources/views/vendor/wallet-request.blade.php
@@ -23,7 +23,7 @@
             <div class="col-lg-12">
                 <div class="dashboard__card card__two">
                     <div class="dashboard__card__header">
-                        <h4 class="dashboard__card__title">{{ __('All Withdraw Request') }}</h4>
+                        <h4 class="dashboard__card__title">{{ __('All Withdraw Requests') }}</h4>
                     </div>
                     <div class="dashboard__card__body">
                         <div class="table-wrap">
