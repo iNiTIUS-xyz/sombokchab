c746b80dd4dae0d2c7225aa52bc1a4374b7a9c21
Masud Rana Tapu <masudranatapu@gmail.com>
2025-05-13 23:03:17 +0600

    done check auth vendor then redirect to vendor dashboard

---
+++ core/app/Http/Middleware/CheckAuthVendorMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAuthVendorMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // If the user is authenticated with the 'vendor' guard
+        if (Auth::guard('vendor')->check()) {
+            return redirect()->route('vendor.home');
+        }
+        // Otherwise, continue with the request
+        return $next($request);
+    }
+}
