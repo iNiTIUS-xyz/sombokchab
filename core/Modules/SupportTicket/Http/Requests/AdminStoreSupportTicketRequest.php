a53530a0ac4bced58c1567a1d173999f51059dc8
Masud Rana Tapu <masudranatapu@gmail.com>
2025-07-24 16:55:46 +0600

    done

--- core/Modules/SupportTicket/Http/Requests/AdminStoreSupportTicketRequest.php
+++ core/Modules/SupportTicket/Http/Requests/AdminStoreSupportTicketRequest.php
@@ -17,7 +17,7 @@ class AdminStoreSupportTicketRequest extends FormRequest
             'title' => 'required|string|max:191',
             'subject' => 'required|string|max:191',
             'priority' => 'nullable|string|max:191',
-            'user_type' => 'required',
+            'user_type' => 'nullable',
             'description' => 'required|string',
             'departments' => 'required|string',
         ];
