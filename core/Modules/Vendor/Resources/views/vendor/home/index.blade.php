cfaa09538e57d50a3e521815349bdc9b28d112a6
Masud Rana Tapu <masudranatapu@gmail.com>
2025-08-11 22:13:41 +0600

    demo version done for vendor

--- core/Modules/Vendor/Resources/views/vendor/home/index.blade.php
+++ core/Modules/Vendor/Resources/views/vendor/home/index.blade.php
@@ -41,16 +41,6 @@
                 <div class="col-12">
                     <div class="dashboard__card">
                         <div class="dashboard__card__body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <h3 class="my-3">Webstie Stats</h3>
-                                </div>
-                                <div class="col-md-6 text-end">
-                                    <a href="#" class="text-warning">View All</a>
-                                </div>
-                            </div>
-
-
                             <div class="row g-5 mt-2">
                                 <div class="col-md-6">
                                     <h3 class="my-3">Financial Summary</h3>
