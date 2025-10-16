19b7a889d78b8e6f6fbbd0d1eef21b18c1ffd0d1
Masud Rana Tapu <masudranatapu@gmail.com>
2025-10-17 01:02:28 +0600

    done

--- core/resources/views/vendor/notification.blade.php
+++ core/resources/views/vendor/notification.blade.php
@@ -36,23 +36,44 @@
                             @endphp
 
                             <tr>
-                                <td class="{{ $notification->type == 'stock_out' ? 'bg bg-warning' : '' }}">
-                                    <div class="notification-list-flex  d-flex">
-                                        <div class="notification-icon ml-3">
-                                            <i class="las la-bell"></i>
-                                        </div>
 
-                                        <div class="notification-contents">
-                                            <a class="list-title" href="{{ $href }}">
-                                                {!! str_replace(
-                                                    ['{product_name}', '{vendor_text}'],
-                                                    ["<b>$productName</b>", ''],
-                                                    formatNotificationText(strip_tags($notification->message)),
-                                                ) !!} </a>
-                                            <span class="list-sub"> {{ $notification->created_at->diffForHumans() }} </span>
+                                @if ($notification->type == 'product')
+                                    <td class="{{ $notification->type == 'stock_out' ? 'bg bg-warning' : '' }}">
+                                        <div class="notification-list-flex d-flex">
+                                            <div class="notification-icon ml-3">
+                                                <i class="las la-bell"></i>
+                                            </div>
+                                            <div class="notification-contents">
+                                                <a class="list-title" href="javascript:;"
+                                                    onclick="markAsReadAndRedirect('{{ route('notification.markAsRead', $notification->id) }}', '{{ $href }}')">
+                                                    {!! str_replace(
+                                                        ['{product_name}', '{vendor_text}'],
+                                                        ["<b>$productName</b>", ''],
+                                                        formatNotificationText(strip_tags($notification->message)),
+                                                    ) !!} </a>
+                                                <span class="list-sub">
+                                                    {{ $notification->created_at->diffForHumans() }}
+                                                </span>
+                                            </div>
                                         </div>
-                                    </div>
-                                </td>
+                                    </td>
+                                @else
+                                    <td>
+                                        <div class="notification-list-flex d-flex"
+                                            @if ($notification->is_read_vendor == 0) style="font-weight: bold;" @endif>
+                                            <div class="notification-icon ml-3">
+                                                <i class="las la-bell"></i>
+                                            </div>
+                                            <a class="list-title" href="javascript:;"
+                                                onclick="markAsReadAndRedirect('{{ route('notification.markAsRead', $notification->id) }}', '{{ $href }}')">
+                                                {{ $notification->message }}
+                                                <span class="list-sub">
+                                                    {{ $notification->created_at->diffForHumans() }}
+                                                </span>
+                                            </a>
+                                        </div>
+                                    </td>
+                                @endif
                             </tr>
                         @endforeach
                     </tbody>
@@ -89,5 +110,28 @@
                 });
             }
         });
+    </script>
+
+    <script>
+        function markAsReadAndRedirect(markUrl, redirectUrl) {
+            fetch(markUrl, {
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    'Accept': 'application/json'
+                }
+            }).then(response => {
+
+                if (response.ok) {
+                    window.location.href = redirectUrl;
+                } else {
+                    console.error('Failed to mark as read.');
+                    window.location.href = redirectUrl; // still redirect
+                }
+            }).catch(err => {
+                console.error(err);
+                window.location.href = redirectUrl;
+            });
+        }
     </script>
 @endsection
