2a09369958d8dbb6ee45d8e84d4364f5ad3b5229
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2025-12-09 11:51:27 +0600

    fixed passport and national id field maxLength in vendor register

--- core/Modules/Vendor/Resources/views/vendor/register/index.blade.php
+++ core/Modules/Vendor/Resources/views/vendor/register/index.blade.php
@@ -183,6 +183,7 @@
                                                 <span class="text-danger">*</span>
                                             </label>
                                             <input name="passport_nid" id="passport_nid" type="number" maxlength="30"
+                                                oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0, 30);"
                                                 class="form--control radius-10"
                                                 placeholder="{{ __('Enter Passport or National ID') }}" required />
                                             <small class="text-danger" id="passportNidError"></small>
