660b94cfab4c62f5558c4f9294443f052f9401b8
Masud Rana Tapu <masudranatapu@gmail.com>
2026-01-10 16:11:10 +0600

    done

--- core/Modules/Product/Resources/views/components/frontend/grid-style-05.blade.php
+++ core/Modules/Product/Resources/views/components/frontend/grid-style-05.blade.php
@@ -2,21 +2,13 @@
     $attributes = $product?->inventory_detail_count ?? null;
     $campaign_product = $product->campaign_product ?? null;
 
-    $campaignProductEndDate =
-        $product->campaign->end_date
-        ?? ($product->campaign->end_date->end_date ?? null);
+    $campaignProductEndDate = $product->campaign->end_date ?? ($product->campaign->end_date->end_date ?? null);
 
-    $sale_price = $campaign_product
-        ? optional($campaign_product)->campaign_price
-        : $product->sale_price;
+    $sale_price = $campaign_product ? optional($campaign_product)->campaign_price : $product->sale_price;
 
-    $deleted_price = !is_null($campaign_product)
-        ? $product->sale_price
-        : $product->price;
+    $deleted_price = !is_null($campaign_product) ? $product->sale_price : $product->price;
 
-    $campaign_percentage = !is_null($campaign_product)
-        ? getPercentage($product->sale_price, $sale_price)
-        : false;
+    $campaign_percentage = !is_null($campaign_product) ? getPercentage($product->sale_price, $sale_price) : false;
 
     $stock_count = optional($product->inventory)->stock_count;
     $filter = $filter ?? false;
@@ -32,10 +24,7 @@
             </a>
 
             {{-- Badge & Discount (REUSED COMPONENT) --}}
-            {!! view(
-                'product::components.frontend.common.badge-and-discount',
-                compact('product', 'campaign_percentage')
-            ) !!}
+            {!! view('product::components.frontend.common.badge-and-discount', compact('product', 'campaign_percentage')) !!}
         </div>
 
         {{-- Product Contents --}}
@@ -44,19 +33,13 @@
             {{-- Title --}}
             <h6 class="product__card__contents__title mt-2">
                 <a href="{{ route('frontend.products.single', $product->slug) }}">
-                    {{ langWiseShowValue(
-                        Str::limit($product->name, 25, '...'),
-                        Str::limit($product->name_km, 25, '...')
-                    ) }}
+                    {{ langWiseShowValue(Str::limit($product->name, 25, '...'), Str::limit($product->name_km, 25, '...')) }}
                 </a>
             </h6>
 
             {{-- Rating --}}
             <div class="rating-wrap">
-                <x-product::frontend.common.rating-markup
-                    :rating-count="$product->ratings_count"
-                    :avg-rattings="$product->ratings_avg_rating ?? 0"
-                />
+                <x-product::frontend.common.rating-markup :rating-count="$product->ratings_count" :avg-rattings="$product->ratings_avg_rating ?? 0" />
             </div>
 
             {{-- Price --}}
@@ -65,7 +48,7 @@
                     {{ float_amount_with_currency_symbol(calculatePrice($sale_price, $product)) }}
                 </span>
 
-                @if($deleted_price)
+                @if ($deleted_price)
                     <s class="product__price__old">
                         {{ float_amount_with_currency_symbol(calculatePrice($deleted_price, $product)) }}
                     </s>
@@ -75,25 +58,22 @@
             {{-- Cart Actions --}}
             <div class="product__card__cart mt-3">
                 @if ($stock_count <= 0)
-                    <button type="button"
-                            class="product__card__cart__outline radius-30 out-of-stock-btn"
-                            disabled>
+                    <button type="button" class="product__card__cart__outline radius-30 out-of-stock-btn" disabled>
                         {{ __('Out of Stock') }}
                     </button>
                 @else
                     @if (isset($attributes) && $attributes > 0)
-                        <a href="{{ route('frontend.products.single', $product->slug) }}"
-                           class="product__card__cart__btn radius-30"
-                           title="{{ __('View Details') }}">
-                            {{ __('View Details') }}
+                        <a href="#1" data-id="{{ $product->id }}" data-attributes="{{ $product->attribute }}"
+                            data-old-text="{{ __('Add To Cart') }}"
+                            class="product__card__cart__outline radius-30 add_to_cart_ajax"
+                            title="{{ __('Add To Cart') }}">
+                            {{ __('Add To Cart') }}
                         </a>
                     @else
-                        <a href="#1"
-                           data-id="{{ $product->id }}"
-                           data-attributes="{{ $product->attribute }}"
-                           data-old-text="{{ __('Add To Cart') }}"
-                           class="product__card__cart__outline radius-30 add_to_cart_ajax"
-                           title="{{ __('Add To Cart') }}">
+                        <a href="#1" data-id="{{ $product->id }}" data-attributes="{{ $product->attribute }}"
+                            data-old-text="{{ __('Add To Cart') }}"
+                            class="product__card__cart__outline radius-30 add_to_cart_ajax"
+                            title="{{ __('Add To Cart') }}">
                             {{ __('Add To Cart') }}
                         </a>
                     @endif
@@ -101,25 +81,20 @@
 
                 {{-- Right Icons --}}
                 <div class="product__card__cart__right">
-                    <a href="javascript:void(0)"
-                       data-id="{{ $product->id }}"
-                       title="{{ __('Add To Compare') }}"
-                       class="product__card__cart__btn__icon cart-loading icon add_to_compare_ajax">
+                    <a href="javascript:void(0)" data-id="{{ $product->id }}" title="{{ __('Add To Compare') }}"
+                        class="product__card__cart__btn__icon cart-loading icon add_to_compare_ajax">
                         <i class="las la-retweet"></i>
                     </a>
 
                     @if (isset($attributes) && $attributes > 0)
-                        <a href="#1"
-                           title="{{ __('View Details') }}"
-                           class="product-quick-view-ajax favourite icon cart-loading product__card__cart__btn__icon"
-                           data-action-route="{{ route('frontend.products.single-quick-view', $product->slug) }}">
+                        <a href="#1" title="{{ __('View Details') }}"
+                            class="product-quick-view-ajax favourite icon cart-loading product__card__cart__btn__icon"
+                            data-action-route="{{ route('frontend.products.single-quick-view', $product->slug) }}">
                             <i class="lar la-save"></i>
                         </a>
                     @else
-                        <a href="#1"
-                           data-id="{{ $product->id }}"
-                           title="{{ __('Save For Later') }}"
-                           class="add_to_wishlist_ajax icon cart-loading product__card__cart__btn__icon">
+                        <a href="#1" data-id="{{ $product->id }}" title="{{ __('Save For Later') }}"
+                            class="add_to_wishlist_ajax icon cart-loading product__card__cart__btn__icon">
                             <i class="lar la-save"></i>
                         </a>
                     @endif
