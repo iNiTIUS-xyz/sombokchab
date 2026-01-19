973e9a48216b5816f0d0f2013fc30bf694db0cfb
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-01-19 10:40:18 +0600

    fixed coupon discount field validation

--- core/Modules/Product/Resources/views/backend/coupon/edit.blade.php
+++ core/Modules/Product/Resources/views/backend/coupon/edit.blade.php
@@ -152,8 +152,8 @@
                             <div class="col-md-6 mb-3">
                                 <label>{{ __('Discount') }} <span class="text-danger">*</span></label>
                                 <input type="number" class="form-control discount" id="edit_discount" step="0.01"
-                                    placeholder="Enter Discount" name="discount" required pattern="[0-9]+(\.[0-9]{1,2})?"
-                                    value="{{ $coupon->discount }}">
+                                    max="99..99" placeholder="Enter Discount" name="discount" required
+                                    pattern="[0-9]+(\.[0-9]{1,2})?" value="{{ $coupon->discount }}">
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label>{{ __('Expire Date') }} <span class="text-danger">*</span></label>
@@ -271,6 +271,23 @@
 
 
 
+        });
+    </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const discount = document.querySelector('.discount');
+
+            discount.addEventListener('input', function() {
+                let value = parseFloat(this.value);
+
+                if (value > 99.99) {
+                    this.value = 99.99;
+                }
+
+                if (value < 1) {
+                    this.value = '';
+                }
+            });
         });
     </script>
 @endsection
